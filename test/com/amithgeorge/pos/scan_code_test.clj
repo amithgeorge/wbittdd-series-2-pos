827fc642a19893985bcff5c47a9b981211e8c7a7
Amith George <strider.am@gmail.com>
2021-12-03 19:06:40 +0530

    refactor scan code test to use common setup

--- test/com/amithgeorge/pos/scan_code_test.clj
+++ test/com/amithgeorge/pos/scan_code_test.clj
@@ -12,26 +12,28 @@
         "product 2" "USD 37.19"}
        product-id))
 
+(defn setup
+  []
+  (let [display-text (atom "DISPLAY_NOT_CALLED")
+        display-fn (fn [msg] (reset! display-text msg))
+        sut-fn (partial sut/scan price-fn display-fn)]
+    [sut-fn display-text]))
+
 (deftest handle-code-for-existing-product
   (testing "Given a valid code for an existing product, display its price"
-    (let [input "product-1\n"
-          display-text (atom "DISPLAY_NOT_CALLED")
-          display-fn (fn [msg] (reset! display-text msg))]
-      (sut/scan price-fn display-fn input)
+    (let [[sut-fn display-text] (setup)]
+      (sut-fn "product-1\n")
       (is (= "USD 15.50" @display-text))))
 
   (testing "Given a valid code for another existing product, display its price"
-    (let [input "product 2\n"
-          display-text (atom "DISPLAY_NOT_CALLED")
-          display-fn (fn [msg] (reset! display-text msg))]
-      (sut/scan price-fn display-fn input)
+    (let [[sut-fn display-text] (setup)]
+      (sut-fn "product 2\n")
       (is (= "USD 37.19" @display-text)))))
 
 (deftest handle-code-for-product-not-found
   (testing "Given a valid code for product whose info we don't have, display not found message"
-    (let [display-text (atom "DISPLAY_NOT_CALLED")
-          display-fn (fn [msg] (reset! display-text msg))]
-      (sut/scan price-fn display-fn "product-random-id\n")
+    (let [[sut-fn display-text] (setup)]
+      (sut-fn "product-random-id\n")
       (is (= "Not found!" @display-text)))))
 
 (deftest handle-malformed-code
@@ -44,20 +46,17 @@
                    ["Invalid code!" "product-1  " "code with no newline is invalid"]]]
     (testing "malformed inputs"
       (doseq [[expected input err-message] args-list]
-        (let [display-text (atom "DISPLAY_NOT_CALLED")
-              display-fn (fn [msg] (reset! display-text msg))]
-          (sut/scan price-fn display-fn input)
+        (let [[sut-fn display-text] (setup)]
+          (sut-fn input)
           (is (= expected @display-text) err-message))))))
 
 (deftest support-both-unix-and-windows-newlines
   (testing "A code ending with unix newline is treated as valid"
-    (let [display-text (atom "DISPLAY_NOT_CALLED")
-          display-fn (fn [msg] (reset! display-text msg))]
-      (sut/scan price-fn display-fn "product-1\n")
+    (let [[sut-fn display-text] (setup)]
+      (sut-fn "product-1\n")
       (is (valid-price-message? @display-text))))
 
   (testing "A code ending with windows newline is treated as valid"
-    (let [display-text (atom "DISPLAY_NOT_CALLED")
-          display-fn (fn [msg] (reset! display-text msg))]
-      (sut/scan price-fn display-fn "product-1\r\n")
+    (let [[sut-fn display-text] (setup)]
+      (sut-fn "product-1\r\n")
       (is (valid-price-message? @display-text)))))
