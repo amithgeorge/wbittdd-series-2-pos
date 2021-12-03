ac1088b83872506decb051b247afbec680100afe
Amith George <strider.am@gmail.com>
2021-12-03 12:35:35 +0530

    fix build script

--- build.clj
+++ build.clj
@@ -3,14 +3,15 @@
   (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
             [org.corfield.build :as bb]))
 
+(def lib 'com.amithgeorge/pos)
 (def version (format "1.0.%s" (b/git-count-revs nil)))
-(def main 'com.amithgeorge.pos)
+(def main 'com.amithgeorge.pos.main)
 
 (defn uberjar
   "Create the uberjar"
   [opts]
   (println "Creating the uberjar")
   (-> opts
-      (assoc :version version :main main)
+      (assoc :lib lib :version version :main main)
       (bb/clean)
       (bb/uber)))
