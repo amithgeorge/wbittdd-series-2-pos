28660ddca33b7c4ffde2fd05dee73dc6f7787e1f
Amith George <strider.am@gmail.com>
2021-12-05 18:43:23 +0530

    move cart functions to cart ns

--- src/com/amithgeorge/pos/cart.clj
+++ src/com/amithgeorge/pos/cart.clj
@@ -2,3 +2,16 @@
 
 (defn new []
   (atom {:total 0M}))
+
+(defn update-total
+  [cart product-price]
+  (swap! cart update :total + product-price))
+
+(defn empty?
+  [cart]
+  (zero? (get @cart :total)))
+
+(defn total
+  [cart]
+  (get @cart :total))
+
--- src/com/amithgeorge/pos/pos.clj
+++ src/com/amithgeorge/pos/pos.clj
@@ -1,6 +1,7 @@
 (ns com.amithgeorge.pos.pos
   (:require [clojure.string :as str]
-            [com.amithgeorge.pos.display :as display]))
+            [com.amithgeorge.pos.display :as display]
+            [com.amithgeorge.pos.cart :as cart]))
 
 (defn- ends-with-newline? [input]
   (or (.endsWith input "\n")
@@ -17,31 +18,17 @@
         nil
         code))))
 
-;; Cart functions
-
-(defn- update-cart-total
-  [cart product-price]
-  (swap! cart update :total (fnil + 0M) product-price))
-
-(defn- cart-empty?
-  [cart]
-  (zero? (get @cart :total)))
-
-(defn- cart-total
-  [cart]
-  (get @cart :total))
-
 (defn scan
   ([price display cart input]
    (if-let [product-id (parse-product-id input)]
      (if-let [product-price (price product-id)]
        (do (display/price display product-price)
-           (update-cart-total cart product-price))
+           (cart/update-total cart product-price))
        (display/not-found-message display))
      (display/code-invalid-message display))))
 
 (defn total
   [display cart]
-  (if (cart-empty? cart)
+  (if (cart/empty? cart)
     (display/nothing-scanned-message display)
-    (display/total display (cart-total cart))))
+    (display/total display (cart/total cart))))
